ab3be1b2a61ffbe217e6b81984d10ae68332d660
Chris Keller <kellerch@umich.edu>
2020-05-10 16:14:32 -0400

    Add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // Minify CSS if in production environment.
+    process.env.NODE_ENV === "production" ? cssnano : "",
+  ],
 };
